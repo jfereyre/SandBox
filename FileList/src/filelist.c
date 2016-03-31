cf45a89f440ee55def3abe8fa054e9e9a6d812c6
jerome fereyre <jfereyre@gmail.com>
2016-03-31 16:05:58 +0200

    Fix compilation issues

--- FileList/src/filelist.c
+++ FileList/src/filelist.c
@@ -15,22 +15,7 @@ int main(int argc, char *argv[]) {
 	if ( l_fileList != NULL ) {
 		
 		t_linkedList * l_directoryList = linkedListCreate();
-		// linkedListPush(l_directoryList, "C:\\Users\\A624072\\Downloads\\lubuntu1210");
-		// linkedListPush(l_directoryList, "C:\\Users\\A624072\\Downloads\\systor");
-		linkedListPush(l_directoryList, "/bin");
-		linkedListPush(l_directoryList, "/boot");
-		linkedListPush(l_directoryList, "/etc");
-		linkedListPush(l_directoryList, "/home");
-		linkedListPush(l_directoryList, "/initrd.img");
-		linkedListPush(l_directoryList, "/lib");
-		linkedListPush(l_directoryList, "/lib64");
-		linkedListPush(l_directoryList, "/opt");
-		linkedListPush(l_directoryList, "/root");
-		linkedListPush(l_directoryList, "/sbin");
-		linkedListPush(l_directoryList, "/srv");
-		linkedListPush(l_directoryList, "/usr");
-		linkedListPush(l_directoryList, "/var");
-		linkedListPush(l_directoryList, "/vmlinuz");
+		linkedListPush(l_directoryList, "C:\\");
 		LOG_DEBUG("before dumpJSONObject call.");
 		printf("%s", stringBufferGetString(dumpJSONObject(exploreDirectories(l_directoryList))));
 		LOG_DEBUG("after dumpJSONObject call.");
